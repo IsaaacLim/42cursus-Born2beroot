d02aa77d3ff23946b5c26d099e900f057c05e65c
Isaac Lim <75291303+IsaaacLim@users.noreply.github.com>
2021-06-03 15:11:06 +0800

    Create phpconfig.php

---
+++ phpconfig.php
@@ -0,0 +1,8 @@
+define('AUTH_KEY',         'qfG%B5]RK/:g+3Izo<(fA)$dsOXBr-a=jxiLi;|0h>[]i!(7wk<tw|qoHiw_br/9');
+define('SECURE_AUTH_KEY',  ' `(-D8qD{tpGuQ=92{Sz3`jP^8#pR4g]HKD{ZEWSR/gCx]{2G,^wQ?q>2-k3`-iY');
+define('LOGGED_IN_KEY',    '[.te`{@j;a*Ye(G2s-x6P~8+MKqgaV5T@<+;U&jh|33<9r3-j,pk+Ge#JFF$$u7~');
+define('NONCE_KEY',        'OP.?8=:|Qs=|q&s$En.e-tp/hG>:$1gg*#VF},-um>3-4f(YlJ*ulAZU!*1rm*u*');
+define('AUTH_SALT',        'rR@J!OaYp]+VgK:sp^0k+PXMA0V.p/xIE9%1{]]$55V$pNLJqQ+|[x^XN8qm>]a*');
+define('SECURE_AUTH_SALT', 'n VwIAN[Xi_G81(-7eR&]d]UFF%k%Nb^wLR9KN r-ru[2^>$8|w,:#mR5b!DT)jF');
+define('LOGGED_IN_SALT',   'FG$:.8iz,p{P`9V33g]qao:|[&w?(o!h:+,@nY)x7Fm>9}s*68xE}!A]-$=VwwO#');
+define('NONCE_SALT',       'H-krKU0]GYl#,2+z+|7~?hgu;3x4&i-^K^HbwHJK7CmNT/4X2$+O`XSx.|L^>3qm');
